5cad2cfdea77228113f8ed00eff45a40d1c50227
shahnirav <shah.nirav@me.com>
2017-03-02 08:13:41 +0000

    M

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ if (env === 'dev') {
     });
 }
 
-app.set('port', (process.env.PORT || 12000));
+app.set('port', (process.env.PORT || 8084));
 
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
